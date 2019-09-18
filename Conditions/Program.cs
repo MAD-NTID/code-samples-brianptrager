506e5749c44631fb6962ddf0fa349c42a550256c
Brian Trager <brian.p.trager@outlook.com>
2019-09-18 11:36:18 -0400

    Added Ternary project

---
+++ Conditions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MINIMUM_DRINKING_AGE = 21;
+
+            Console.Write("Enter your age: ");
+
+            if(int.TryParse(Console.ReadLine(), out int age))
+            {
+                if(0 <= age)
+                {
+                    /*if(age >= MINIMUM_DRINKING_AGE)
+                    {
+                        Console.WriteLine("You are legally permitted to drink.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are not allowed to drink any beer or whatever");
+                    }*/
+                    Console.WriteLine((age >= MINIMUM_DRINKING_AGE) ? "Allowed to drink" : "Not allowed to drink");
+                }
+                else
+                {
+                    Console.WriteLine("Negative number is an invalid input...exiting program");
+                    Console.Beep(200,2000);
+                    Console.Beep(1000,1000);
+                    Console.Beep(200,2000);
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input.");
+                Console.Beep(200,2000);
+            }
+        }
+    }
+}
