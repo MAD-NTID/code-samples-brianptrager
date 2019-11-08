8389d3c31db14afa32b79a9ba6ea0cad91ed557a
Brian Trager <bptnbs@ad.rit.edu>
2019-11-08 08:56:42 -0500

    ToString() and Properties

---
+++ Properties/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars = new Car[0];
+
+            Car myCar;
+
+            while(true)
+            {
+                Console.Write("Enter make of car: ");
+                string make = Console.ReadLine();
+
+                Console.Write("Enter model of car: ");
+                string model = Console.ReadLine();
+                
+                Console.Write("Enter year of car: ");
+                int year = int.Parse(Console.ReadLine());
+                
+                Console.Write("Enter color of car: ");
+                string color = Console.ReadLine();
+                myCar = new Car(make, model, year, color);
+
+                Console.Write("Do you wish to create another car? (y/n): ");
+
+                Array.Resize(ref cars, cars.Length + 1);
+                cars[cars.Length - 1] = myCar;
+
+                if(!(Console.ReadLine().ToUpper()[0] == 'Y'))   
+                    break;
+            }
+            for(int i = 0; i < cars.Length; i++)
+            {
+                Car car = cars[i];
+                Console.WriteLine($"##########Car #{i + 1}###################");
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
